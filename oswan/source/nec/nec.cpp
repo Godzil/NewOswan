b662fc05a11a0e7a567fb683247a6d8aeceb207f
Godzil <godzil@godzil.net>
2019-09-20 16:27:50 +0100

    Adding the main code for instruction trace

--- oswan/source/nec/nec.cpp
+++ oswan/source/nec/nec.cpp
@@ -3930,7 +3930,11 @@ int nec_execute(int cycles)
 
    while(nec_ICount>0)
    {
-
+#if 1
+      uint8_t op = cpu_readmem20((I.sregs[CS]<<4) + I.ip);
+      printf("[%04x:%04xh] %02xh '%s' - I=%d\n", I.sregs[CS], I.ip,
+            op, instructionsName[op], I.IF);
+#endif
       nec_instruction[FETCHOP]();
 //		nec_ICount++;
    }
