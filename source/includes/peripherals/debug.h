c68d7dd13496f7924470efa53b5a6461ec3fe3c8
Godzil <godzil@godzil.net>
2022-07-18 17:22:34 +0100

    Debug: make it compliant with device.h

---
+++ source/includes/peripherals/debug.h
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * NewOswan
+ * debug.h:
+ *
+ * Created by Manoel Trapier on 18/07/22.
+ * Copyright (c) 2022 986-Studio. All rights reserved.
+ *
+ ******************************************************************************/
+
+#ifndef __P_DEBUG_H__
+#define __P_DEBUG_H__
+
+#include <device.h>
+
+/* Exported device infos */
+extern device_t DebugPeripheral;
+
+#endif /* __P_DEBUG_H__ */
--- source/peripherals/debug.c
+++ source/peripherals/debug.c
@@ -10,25 +10,68 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint8_t debug_io_read(void *pdata, uint8_t port)
+#include <io.h>
+
+#include <peripherals/debug.h>
+
+typedef struct debug_pdata_t
 {
+    int16_t value;
+    char character;
+} debug_pdata_t;
+
+static debug_pdata_t debug_data;
+
+static uint8_t debug_io_read(void *pdata, uint8_t port)
+{
+    debug_pdata_t *params = (debug_pdata_t *)pdata;
+
     switch (port)
     {
-
+    case 0x0: return params->value & 0xFF;
+    case 0x1: return params->value >> 8;
+    case 0x2: return params->character;
     }
+
+    return 0x90;
 }
 
-void debug_io_write(void *pdata, uint8_t port, uint8_t value)
+static void debug_io_write(void *pdata, uint8_t port, uint8_t value)
 {
+    debug_pdata_t *params = (debug_pdata_t *)pdata;
+
     switch (port)
     {
-    case 0xF1:
-        //printf("%d\n", (signed short)((value << 8) | ws_ioRam[0xF0]));
+    case 0x0:
+        params->value = (params->value & 0xFF00) | value;
         break;
 
-    case 0xF2:
+    case 0x1:
+        params->value = (params->value & 0x00FF) | (value << 8);
+        printf("%d\n", params->value);
+        break;
+
+    case 0x2:
+        params->character = value;
         printf("%c", value);
         fflush(stdout);
         break;
     }
 }
+
+static void debug_init(uint8_t baseAddress, void *params)
+{
+    io_register_hook(baseAddress, 0x0, &debug_data, debug_io_read, debug_io_write);
+    io_register_hook(baseAddress, 0x1, &debug_data, debug_io_read, debug_io_write);
+    io_register_hook(baseAddress, 0x2, &debug_data, debug_io_read, debug_io_write);
+}
+
+/* Exported device */
+device_t InterruptController =
+{
+    .init = debug_init,
+    .update = NULL,
+    .reset = NULL,
+    .free = NULL,
+    .type = DT_DEBUG,
+};
